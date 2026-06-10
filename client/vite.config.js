7964a73651f61a796a67a9fbbbca021788aa2143
sunny <sunnrooy@gmail.com>
2026-06-10 00:22:29 -0700

    fix: resolve vite chunk size warning

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
